307e51cd5dcfcae66d8f43099aea73407ecfb82d
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-08 22:40:43 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -37,10 +37,14 @@ User.update(req.params.id, { name: req.name})
 .catch(next)
 })
 
-router.delete('/:id', validateUserId,  (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user);
+router.delete('/:id', validateUserId, async (req, res, next) => {
+try{
+ await User.remove(req.params.id)
+ res.json(req.user)
+} catch(err) {
+  next(err)
+
+}
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
